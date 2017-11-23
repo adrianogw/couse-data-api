1cfbf5aec1d57719df4c5e745c0d3d80565c85ca
wincka <wincka@MIBDZNMRYL.americas.hpqcorp.net>
2017-11-23 20:51:35 -0200

    Support for the POST and PUT operations via a modal dialog.

--- src/main/resources/static/topicApplication.js
+++ src/main/resources/static/topicApplication.js
@@ -1,5 +1,5 @@
 /**
- * 
+ * Script containing the service methods and controller methods to support all the CRUD operations for the Topic subject.
  */
 
 var helloApp = angular.module( "helloApp", ['ui.bootstrap'] );
@@ -52,9 +52,11 @@ helloApp.service('TopicService', [ '$http', function($http) {
 	}        
 } ]);    
 
-helloApp.controller('HelloCtrl', ['$scope','TopicService', 
-	function ($scope, TopicService) {
+helloApp.controller('HelloCtrl', ['$scope','TopicService', '$uibModal', '$log', '$document',
+	function ($scope, TopicService, $uibModal, $log, $document) {
 
+	$scope.topic = null;
+	
 	$scope.getTopic = function (id) {
 		TopicService.getTopic(id)
 		.then(function success(response) {
@@ -74,12 +76,11 @@ helloApp.controller('HelloCtrl', ['$scope','TopicService',
 	};
 
 	$scope.resetTopic = function () {
-		$scope.topic.id = null;
-		$scope.topic.name = null;
-		$scope.topic.description = null;
+		$scope.topic = null;
 	}
 
 	$scope.addTopic = function () {
+		
 		if ($scope.topic != null && $scope.topic.name) {
 			TopicService.addTopic($scope.topic.id, $scope.topic.name, $scope.topic.description)
 			.then (function success(response){
@@ -143,6 +144,96 @@ helloApp.controller('HelloCtrl', ['$scope','TopicService',
 
 	$scope.getAllTopics();
 
+	//function to open a modal dialog. The controller for the modal dialog is ModalInstanceCtrl (specified below).
+	$scope.openModalDialog = function (crudAction, selectedTopic, size, parentSelector) {
+		
+		var parentElem = parentSelector ? 
+				angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+
+				var modalInstance = $uibModal.open({
+					animation: 'true',
+					ariaLabelledBy: 'modal-title',
+					ariaDescribedBy: 'modal-body',
+					templateUrl: 'myModalContent.html',
+					controller: 'ModalInstanceCtrl',
+					controllerAs: '$ctrl', //Alias that must be used in the HTML code for the modal dialog template
+					size: size,
+					appendTo: parentElem,
+					resolve: {
+						//This function is passed as a parameter to the modal dialog controller.
+						currentTopic: function () {
+							return selectedTopic;
+						}
+					}
+				});
+
+				//Called when the modalInstance is closed. In our case here, it is fired by the $ctrl.ok inside the ModalInstanceCtrl
+				modalInstance.result.then(function (topicFilled) {
+
+					$scope.topic = topicFilled;
+					
+					if (crudAction == 'POST')
+					{
+						$scope.addTopic();
+					}
+					else
+					{
+						$scope.updateTopic();
+					}
+
+				}, function () {
+					$log.info('Modal dismissed at: ' + new Date());
+				});
+	};	
+
 }]); 
 
+/**
+ * Modal dialog controller. It is not the same controller above. It is exclusive to control the dialog models.
+ */
 
+angular.module('ui.bootstrap').controller('ModalInstanceCtrl', function ($uibModalInstance, currentTopic) {
+	
+	var $ctrl = this;
+
+	//Clone the object to not change the currentTopic directly.
+	$ctrl.topic =  angular.copy(currentTopic);
+	$ctrl.isUpdating = $ctrl.topic && $ctrl.topic.id; 
+	$ctrl.errorMessage = '';
+	
+	$ctrl.ok = function () {
+		
+		$ctrl.validateFields();
+		
+		if (!$ctrl.errorMessage)
+		{
+			$uibModalInstance.close($ctrl.topic);
+		}
+	};
+
+	$ctrl.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
+
+	$ctrl.validateFields = function () {
+		
+		$ctrl.errorMessage = null;
+		
+		if (!$ctrl.topic.id)
+		{
+			$ctrl.errorMessage = 'Please, specify a topic ID!';	
+		}
+		
+		if (!$ctrl.topic.name)
+		{
+			$ctrl.errorMessage = 'Please, specify a topic name!';	
+		}
+
+		if (!$ctrl.topic.description)
+		{
+			$ctrl.errorMessage = 'Please, specify a topic description!';	
+		}
+	};
+	
+	
+});
